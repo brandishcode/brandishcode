ea2214bcec84f1c7b6de332e420e58eaf1144248
brandishcode <brandishcode@gmail.com>
2024-09-07 17:01:29 +0900

    Fix the amd graphics option

--- developer/system/amd.nix
+++ developer/system/amd.nix
@@ -1,10 +1,16 @@
 { pkgs, config, lib, ... }:
 
-{
+let isGpu = config.gpu == "amd";
+in {
   config = {
-    hardware.graphics = lib.mkIf (config.gpu == "amd") {
+    boot.kernelModules = lib.mkIf isGpu [ "kvm-amd" ];
+    hardware.graphics = lib.mkIf isGpu {
       extraPackages = with pkgs; [ amdvlk ];
       extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+    };
+    hardware.cpu.amd = lib.mkIf isGpu {
+      updateMicrocode =
+        lib.mkDefault config.hardware.enableRedistributableFirmware;
     };
   };
 }
