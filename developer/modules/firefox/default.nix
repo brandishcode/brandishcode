63d155f0ef2c700c5ef76d14acd462d33b3b6090
brandishcode <brandishcode@gmail.com>
2024-09-11 00:59:40 +0900

    Added new bookmarks

--- developer/modules/firefox/default.nix
+++ developer/modules/firefox/default.nix
@@ -37,6 +37,23 @@
                 }
               ];
             }
+            {
+              name = "neovim";
+              bookmarks = [
+                {
+                  name = "conform.nvim";
+                  url = "https://github.com/stevearc/conform.nvim";
+                }
+                {
+                  name = "nvim-lspconfig";
+                  url = "https://github.com/neovim/nvim-lspconfig";
+                }
+                {
+                  name = "mason-lspconfig.nvim";
+                  url = "https://github.com/williamboman/mason-lspconfig.nvim";
+                }
+              ];
+            }
           ];
         }];
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
