e4e9fca7c54befa692be160b1d19ac6acd9212fa
brandishcode <brandishcode@gmail.com>
2025-03-27 21:38:57 +0900

    Use flake-utils

--- developer/flake.nix
+++ developer/flake.nix
@@ -5,6 +5,7 @@
 
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,90 +16,94 @@
     clefru-nur.url = "github:clefru/nur-packages";
   };
 
-  outputs = { nixpkgs, home-manager, nixvim, nur, clefru-nur, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ nur.overlays.default ];
-        config.allowUnfreePredicate = pkg:
-          builtins.elem (nixpkgs.lib.getName pkg) [ "tokyo-night-v2" ];
-      };
-      nixvim' = {
-        home.packages = [
-          (nixvim.packages.${system}.default.extend {
-            imports = [ nixvim.nixvim-configs.${system}.js ];
-          })
-        ];
-      };
-      nixvimjs' = {
-        home.packages = [
-          (nixvim.packages.${system}.js.overrideAttrs (oldAttrs: {
-            ignoreCollisions = true;
-            postInstall = oldAttrs.postInstall or "" + ''
-              mv $out/bin/nvim $out/bin/nvim-js
-            '';
-          }))
-        ];
-      };
-      options = import ./options-definition.nix;
-      home-manager-options = {
-        username = options.username;
-        homeDirectory = "/home/${options.username}";
-        stateVersion = "24.05";
-      };
-    in {
-      homeConfigurations = {
-        "${options.username}" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            {
-              home = with home-manager-options; {
-                inherit username;
-                inherit homeDirectory;
-                inherit stateVersion;
-              };
-            }
-            options
-            ./options-declaration.nix
-            ./home-manager
-            nixvim'
+  outputs =
+    { nixpkgs, flake-utils, home-manager, nixvim, nur, clefru-nur, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nur.overlays.default ];
+          config.allowUnfreePredicate = pkg:
+            builtins.elem (nixpkgs.lib.getName pkg) [ "tokyo-night-v2" ];
+        };
+        nixvim' = {
+          home.packages = [
+            (nixvim.packages.${system}.default.extend {
+              imports = [ nixvim.nixvim-configs.${system}.js ];
+            })
           ];
         };
-      };
+        nixvimjs' = {
+          home.packages = [
+            (nixvim.packages.${system}.js.overrideAttrs (oldAttrs: {
+              ignoreCollisions = true;
+              postInstall = oldAttrs.postInstall or "" + ''
+                mv $out/bin/nvim $out/bin/nvim-js
+              '';
+            }))
+          ];
+        };
+        options = import ./options-definition.nix;
+        home-manager-options = {
+          username = options.username;
+          homeDirectory = "/home/${options.username}";
+          stateVersion = "24.05";
+        };
+      in {
+        packages = {
+          homeConfigurations = {
+            "${options.username}" = home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+              modules = [
+                {
+                  home = with home-manager-options; {
+                    inherit username;
+                    inherit homeDirectory;
+                    inherit stateVersion;
+                  };
+                }
+                options
+                ./options-declaration.nix
+                ./home-manager
+                nixvim'
+              ];
+            };
+          };
 
-      nixosConfigurations = {
-        "${options.username}@${options.hostname}" = nixpkgs.lib.nixosSystem {
-          inherit pkgs;
-          modules = [
-            options
-            ./options-declaration.nix
-            ./system
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = {
-                inherit system;
-                inherit clefru-nur;
-              };
-
-              # home-manager setup
-              home-manager.users.${options.username} = {
-                home.stateVersion = home-manager-options.stateVersion;
-                imports = [
+          nixosConfigurations = {
+            "${options.username}@${options.hostname}" =
+              nixpkgs.lib.nixosSystem {
+                inherit pkgs;
+                modules = [
                   options
                   ./options-declaration.nix
-                  ./home-manager
-                  ./home-manager/sway
-                  nixvim'
-                  nixvimjs'
-                  ./nurs
+                  ./system
+                  home-manager.nixosModules.home-manager
+                  {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.extraSpecialArgs = {
+                      inherit system;
+                      inherit clefru-nur;
+                    };
+
+                    # home-manager setup
+                    home-manager.users.${options.username} = {
+                      home.stateVersion = home-manager-options.stateVersion;
+                      imports = [
+                        options
+                        ./options-declaration.nix
+                        ./home-manager
+                        ./home-manager/sway
+                        nixvim'
+                        nixvimjs'
+                        ./nurs
+                      ];
+                    };
+                  }
                 ];
               };
-            }
-          ];
+          };
         };
-      };
-    };
+      });
 }
