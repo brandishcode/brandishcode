638589b11ed7ebed0a7098e9a19af2c18f159405
brandishcode <brandishcode@gmail.com>
2025-04-02 23:18:02 +0900

    Updated sddm configuration

--- developer/configurations/desktop-environment.nix
+++ developer/configurations/desktop-environment.nix
@@ -1,6 +1,24 @@
-{
+{ pkgs }:
+let
+  theme = (import ./theme.nix).theme;
+  wallpaper = theme.wallpaper;
+  sddm-astronaut-custom = pkgs.sddm-astronaut.override {
+    themeConfig = {
+      Background = "${wallpaper}";
+      FormPosition = "left";
+    };
+  };
+in {
   desktopEnvironment = {
-    sway = true;
-    displayManager = "sddm";
+    sway = false;
+    displayManager = {
+      enable = true;
+      name = "sddm";
+      extraConfig = {
+        themePackages = [ sddm-astronaut-custom ];
+        themeName = "sddm-astronaut-theme";
+        qtPackages = [ pkgs.kdePackages.qtmultimedia ];
+      };
+    };
   };
 }
