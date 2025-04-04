fd8f697c6a96831b1b959b26e6e80b32528123f6
brandishcode <brandishcode@gmail.com>
2025-04-04 22:06:38 +0900

    Update desktop environment configuration

--- developer/configurations/desktop-environment.nix
+++ developer/configurations/desktop-environment.nix
@@ -24,14 +24,20 @@ in {
       {
         label = icons.terminal;
         inherit (builtins.elemAt monitor 0) output;
+        shortcut = 0;
+        apps = [ pkgs.foot ];
       }
       {
         label = icons.browser;
         inherit (builtins.elemAt monitor 1) output;
+        shortcut = 9;
+        apps = [ pkgs.firefox ];
       }
       {
         label = icons.devBrowser;
         inherit (builtins.elemAt monitor 2) output;
+        shortcut = 8;
+        apps = [ pkgs.chromium ];
       }
     ];
   };
