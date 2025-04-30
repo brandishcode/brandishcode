bed4912a8e10a0dcd1ffe2a384dc2700e5fa9e33
brandishcode <brandishcode@gmail.com>
2025-04-30 21:28:50 +0900

    Ricing: waybar - Added calendar - Added clock - Added network

--- developer/desktop-environment/waybar/default.nix
+++ developer/desktop-environment/waybar/default.nix
@@ -6,36 +6,87 @@
       programs.waybar = {
         enable = true;
         settings = {
-          mainBar = {
+          mainBar = with config.theme.colors; {
             layer = "top";
             position = "bottom";
-            output = [ "DP-2" "DP-3" "HDMI-A-1" ];
+            height = 10;
+            output = [ "DP-2" "DP-1" "HDMI-A-1" ];
             modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
             modules-center = [ "sway/window" ];
-            modules-right = [ "network" "temperature" "clock" ];
-
+            modules-right = [
+              "network"
+              "clock#paris"
+              "clock#manila"
+              "clock#tokyo"
+              "clock#calendar"
+              "clock"
+            ];
             "sway/workspaces" = {
               disable-scroll = true;
               all-outputs = true;
             };
-            # "custom/hello-from-waybar" = {
-            #   format = "hello {}";
-            #   max-length = 40;
-            #   interval = "once";
-            #   exec = pkgs.writeShellScript "hello-from-waybar" ''
-            #     echo "from within waybar"
-            #   '';
-            # };
+            network = {
+              format-icons = [ "󰈀" ];
+              format = lib.concatStrings [
+                ''<span size="large" color="${red}">{icon} </span>''
+                ''<span style="oblique" weight="bold">{ifname} </span>''
+                "{ipaddr}"
+              ];
+            };
+            clock = {
+              format = lib.concatStrings [
+                ''<span size="large" color="${yellow}"> </span>''
+                "{0:%H:%M}"
+                ''
+                  <span size="small" rise="-1pt" color="${green}" weight="bold"> {0:%Z}</span>''
+              ];
+            };
+            "clock#calendar" = {
+              format = lib.concatStrings [
+                ''<span size="large" color="${blue}" weight="bold"> </span>''
+                "{0:%Y/%m/%d}"
+              ];
+            };
+            "clock#tokyo" = {
+              timezone = "Asia/Tokyo";
+              format = lib.concatStrings [
+                "{0:%H:%M}"
+                ''
+                  <span size="small" rise="-1pt" color="${green}" weight="bold"> {0:%Z}</span>''
+              ];
+            };
+            "clock#paris" = {
+              timezone = "Europe/Paris";
+              format = lib.concatStrings [
+                "{0:%H:%M}"
+                ''
+                  <span size="small" rise="-1pt" color="${green}" weight="bold"> {0:%Z}</span>''
+              ];
+            };
+            "clock#manila" = {
+              timezone = "Asia/Manila";
+              format = lib.concatStrings [
+                "{0:%H:%M}"
+                ''
+                  <span size="small" rise="-1pt" color="${green}" weight="bold"> {0:%Z}</span>''
+              ];
+            };
+
           };
         };
         style = with config.theme.colors; ''
-          #temperature {
-            background: ${green};
-            color: ${black};
+          * {
+            font-size: 10px;
           }
+          window#waybar {
+            background: transparent;
+          }
+          #clock,
           #network {
-            background: ${blue};
-            color: ${black};
+            padding: 0 5px 0;
+            background: ${background};
+            color: ${foreground};
+            margin: 0 2px 2px;
           }
         '';
       };
