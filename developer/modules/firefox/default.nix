65e6fc02c6eae5f16cdce2b59b6169d98b43e4f2
brandishcode <brandishcode@gmail.com>
2025-05-02 21:30:08 +0900

    Optimize: Use memory cache in firefox instead of disk

--- developer/modules/firefox/default.nix
+++ developer/modules/firefox/default.nix
@@ -89,6 +89,12 @@
           "extensions.autoDisableScopes" = 0;
           "layout.css.prefers-color-scheme.content-override" =
             0; # force to dark mode
+          "browser.cache.disk.enable" = false;
+          "browser.cache.disk.smart_size.enabled" = false;
+          "browser.cache.disk_cache_ssl" = false;
+          "browser.cache.offline.enable" = false;
+          "browser.cache.memory.enable" = true;
+          "browser.cache.memory.capacity" = 1024000;
         };
       };
     };
