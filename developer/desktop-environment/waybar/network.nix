4032ebe12ffbfd13a082e1dd2651703a419f9ce2
brandishcode <brandishcode@gmail.com>
2025-05-10 19:08:02 +0900

    Ricing: added wifi format for network module in waybar

--- developer/desktop-environment/waybar/network.nix
+++ developer/desktop-environment/waybar/network.nix
@@ -1,11 +1,16 @@
 { lib, config }:
 
-with config.theme.colors; {
-  default = {
-    format-ethernet = lib.concatStrings [
-      ''<span size="large" color="${green}">󰈀 </span>''
+with config.theme.colors;
+let
+  createFormat = x:
+    lib.concatStrings [
+      ''<span size="large" color="${green}">${x} </span>''
       "{ifname}"
     ];
+in {
+  default = {
+    format-wifi = createFormat "󰖩 ";
+    format-ethernet = createFormat "󰈀 ";
     tooltip-format-ethernet = lib.concatStrings [ "{ipaddr}" ];
   };
 }
