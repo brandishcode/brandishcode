2161c3b8799fc516097c2646436037e114dfa555
brandishcode <brandishcode@gmail.com>
2025-05-25 19:49:27 +0900

    Configuration: Firefox follow system theme on browser content

--- developer/modules/firefox/settings.nix
+++ developer/modules/firefox/settings.nix
@@ -1,14 +1,13 @@
 {
   "browser.startup.homepage" = "https://github.com/brandishcode";
   "extensions.autoDisableScopes" = 0;
-  "layout.css.prefers-color-scheme.content-override" = 0; # force to dark mode
+  "layout.css.prefers-color-scheme.content-override" = 2;
   "browser.cache.disk.enable" = false;
   "browser.cache.disk.smart_size.enabled" = false;
   "browser.cache.disk_cache_ssl" = false;
   "browser.cache.offline.enable" = false;
   "browser.cache.memory.enable" = true;
   "browser.cache.memory.capacity" = 1024000;
-  "browser.theme.content-theme" = 0;
   "browser.theme.toolbar-theme" = 0;
   "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
   "browser.tabs.allow_transparent_browser" = true;
@@ -20,4 +19,6 @@
   "gfx.webrender.all" = true;
   "svg.context-properties.content.enabled" = true;
   "browser.sessionstore.resume_from_crash" = false;
+  "signon.rememberSignons" = false;
+  "services.sync.prefs.sync.signon.rememberSignons" = false;
 }
