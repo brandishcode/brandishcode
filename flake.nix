25661b6e75914270e464e2716e8484301232789a
brandishcode <brandishcode@gmail.com>
2025-05-31 22:52:27 +0900

    configuration: - Moved from `nix` to `lix` - Update flake - Clean up of unused packages from `clefru`

--- flake.nix
+++ flake.nix
@@ -1,22 +1,26 @@
 {
-  description = "Graphical nix configuration";
-
+  description = "brandishcode Nixos configuration";
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nixvim.url = "github:brandishcode/nixvim-configuration";
+    nixvim = {
+      url = "github:brandishcode/nixvim-configuration";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nur.url = "github:nix-community/nur";
-    clefru-nur.url = "github:clefru/nur-packages";
+    lix-module = {
+      url =
+        "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, flake-utils, home-manager, nixvim, nur, clefru-nur, ... }:
+    { nixpkgs, flake-utils, home-manager, nixvim, nur, lix-module, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -44,6 +48,7 @@
               nixpkgs.lib.nixosSystem {
                 inherit pkgs;
                 modules = [
+                  lix-module.nixosModules.default
                   { _module.args = myArgs; }
                   home-manager.nixosModules.home-manager
                   options
@@ -57,7 +62,6 @@
                     home-manager.useUserPackages = true;
                     home-manager.extraSpecialArgs = {
                       inherit system;
-                      inherit clefru-nur;
                     } // myArgs;
                     # home-manager setup
                     home-manager.users.${options.username} = {
@@ -67,7 +71,6 @@
                         ./options-declaration.nix
                         ./home-manager
                         nixvim'
-                        ./nurs
                       ];
                     };
                   }
--- nurs/clefru/default.nix
+++
@@ -1,1 +0,0 @@
-{ imports = [ ./ib-tws.nix ]; }
