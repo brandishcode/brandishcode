bd128c4bd96f9ec664d2c7ca60475a59a140864f
brandishcode <brandishcode@gmail.com>
2025-06-11 23:34:52 +0900

    home-manager: move session variable in a separate ./environment module

---
+++ environment/default.nix
@@ -0,0 +1,1 @@
+{ imports = [ ./session-variables ]; }
--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,31 +1,18 @@
-{ config, ... }:
+{ ... }:
 
 {
-  config = {
-    home = {
-      sessionVariables = {
-        EDITOR = "nvim";
-      };
-
-      shellAliases = {
-        hms = ''
-          home-manager switch -b backup --flake "path:$(readlink -f /home/${config.user.username}/${config.configpath}/#${config.user.username})"
-        '';
-      };
+  programs = {
+    home-manager.enable = true;
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
     };
-
-    programs = {
-      home-manager.enable = true;
-      direnv = {
-        enable = true;
-        enableBashIntegration = true;
-        nix-direnv.enable = true;
-      };
-      bash.enable = true;
-    };
+    bash.enable = true;
   };
 
   imports = [
     ../packages
+    ../environment
   ];
 }
