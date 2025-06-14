6be6b856f7cb5f8f38d4d215e7acf1395e3db5ae
brandishcode <brandishcode@gmail.com>
2025-06-14 19:25:14 +0900

    bash: add git-prompt.sh

--- packages/programs/bash/default.nix
+++ packages/programs/bash/default.nix
@@ -1,8 +1,11 @@
+{ pkgs, ... }:
+
 {
   programs.bash = {
     enable = true;
     initExtra = ''
-      export PS1='\033[1;32m\]$(whoami):''${PWD/*\//}# \033[00m\]'
+      source ${pkgs.git}/share/bash-completion/completions/git-prompt.sh
+      export PS1='\[\033[0;32m\][\u@\h \W\[\033[1;36m\]$(__git_ps1 " %s")\[\033[0;32m\]]\$\[\033[0;00m\] '
     '';
     enableCompletion = true;
   };
