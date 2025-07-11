b0428f91cdabf192bd73709c5385e16c5934727c
brandishcode <brandishcode@gmail.com>
2025-07-11 19:46:05 +0900

    add packages nixessitycore

--- packages/packages/default.nix
+++ packages/packages/default.nix
@@ -1,5 +1,9 @@
-inputs@{ ... }:
+inputs@{ bcpkgs, ... }:
 
+let
+  neovim = import ./neovim inputs;
+  nixessitycore = bcpkgs.nixessitycore;
+in
 {
-  home.packages = [ (import ./neovim inputs) ];
+  home.packages = [ neovim nixessitycore ];
 }
