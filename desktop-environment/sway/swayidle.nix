704ba2cfcef62b1a7b3caa51db9e0eb82589f74b
brandishcode <brandishcode@gmail.com>
2025-07-26 17:38:23 +0900

    sleep and lock on 5 mins

--- desktop-environment/sway/swayidle.nix
+++ desktop-environment/sway/swayidle.nix
@@ -2,18 +2,37 @@
 
 {
   config = {
-    services.swayidle = {
-      enable = true;
-      timeouts = [
-        {
-          timeout = 600;
-          command = "${pkgs.swaylock}/bin/swaylock -i ${config.theme.wallpaper}";
-        }
-        {
-          timeout = 3600;
-          command = "systemctl suspend";
-        }
-      ];
-    };
+    services.swayidle =
+      let
+        lock = "${pkgs.swaylock}/bin/swaylock -i ${config.theme.wallpaper} --daemonize";
+        display = status: "${pkgs.sway}/bin/swaymsg 'output * power ${status}'";
+      in
+      {
+        enable = true;
+        timeouts = [
+          {
+            timeout = 300;
+            command = "${pkgs.systemd}/bin/systemctl suspend";
+          }
+        ];
+        events = [
+          {
+            event = "before-sleep";
+            command = (display "off") + "; " + lock;
+          }
+          {
+            event = "after-resume";
+            command = display "on";
+          }
+          { 
+            event = "lock";
+            command = (display "off") + "; " + lock;
+          }
+          {
+            event = "unlock";
+            command = display "on";
+          }
+        ];
+      };
   };
 }
