e4f11940d88cd5dc72d545cdec50c3cbe2663a33
brandishcode <brandishcode@gmail.com>
2025-10-13 21:02:58 +0900

    remove the deprecated hardware.graphics.extraPackages and hardware.graphics.extraPackages32

--- system/amd.nix
+++ system/amd.nix
@@ -11,10 +11,6 @@ in
 {
   config = {
     boot.kernelModules = lib.mkIf isGpu [ "kvm-amd" ];
-    hardware.graphics = lib.mkIf isGpu {
-      extraPackages = with pkgs; [ amdvlk ];
-      extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
-    };
     hardware.cpu.amd = lib.mkIf isGpu {
       updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     };
