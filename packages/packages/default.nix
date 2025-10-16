a67555c0c20ba166289d73ab92bc46821a9bcd8e
brandishcode <brandishcode@gmail.com>
2025-10-16 21:44:05 +0900

    added calibre application

--- packages/packages/default.nix
+++ packages/packages/default.nix
@@ -2,12 +2,14 @@ inputs@{ bcpkgs, pkgs, ... }:
 
 let
   neovim = import ./neovim inputs;
+  calibre = import ./calibre inputs;
   nixessitycore = bcpkgs.nixessitycore;
 in
 {
   home.packages = [
     neovim
     nixessitycore
+    calibre
   ]
   ++ (with pkgs; [ npins ]);
 }
